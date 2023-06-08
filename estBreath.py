54dd95b10f815a67741c0ebd03c80c34f04487e0
Sciroccogti <scirocco_gti@yeah.net>
2023-06-08 21:58:20 +0800

    feat: weighted mean

--- estBreath.py
+++ estBreath.py
@@ -3,7 +3,7 @@
 @author Sciroccogti (scirocco_gti@yeah.net)
 @brief 
 @date 2023-06-08 13:40:24
-@modified: 2023-06-08 19:02:29
+@modified: 2023-06-08 21:43:40
 '''
 
 import numpy as np
@@ -17,11 +17,11 @@ def estChusai(Cfg: dict, CSI: np.ndarray, iSamp: int = 0) -> np.ndarray:
     :iSamp: 本次估计Sample集合中第iSamp个样本
     :return:呼吸率估计结果， 长度为Np的numpy数组
     '''
-    
+    # np.save("CSI%d.npy" % iSamp, CSI)
     Np, Nrx, Ntx, Nsc, Nt, Tdur = Cfg["Np"][iSamp], Cfg["Nrx"], Cfg["Ntx"], Cfg["Nsc"], Cfg["Nt"][iSamp], Cfg["Tdur"][iSamp]
 
     fs = Nt / Tdur
-    BPMresol = 0.1
+    BPMresol = 1
     resol = BPMresol / 60 # 要分辨出0.1BPM，需要的频率分辨率
     Ndft = int(fs / resol) # DFT点数
     # print(Ndft)
@@ -40,23 +40,52 @@ def estChusai(Cfg: dict, CSI: np.ndarray, iSamp: int = 0) -> np.ndarray:
     for rx in range(Nrx):
         for tx in range(Ntx):
             for sc in range(Nsc):
-                H[rx][tx][sc] = Wdft @ CSI[rx][tx][sc]
+                H[rx][tx][sc] = Wdft @ CSI[rx][tx][sc] # 用 DFT 变换到频域
                 Hs += H[rx][tx][sc]
 
     filteredHs = np.zeros_like(Hs)
     ret = []
-    BPMrange = [[10, 30], [6, 40], [0, 60]]
+    BPMrange = [[10, 30], [6, 40], [5, 50]]
     BPMrangeI = 0
-    while len(ret) < Np:
-        minBPM, maxBPM = BPMrange[BPMrangeI]
-        BPMrangeI += 1
-        minIndex, maxIndex = int(minBPM / BPMresol), int(maxBPM / BPMresol)
-        filteredHs[minIndex: maxIndex] = Hs[minIndex:maxIndex]
-        idx, _ = find_peaks(np.abs(filteredHs), distance = 5)
+    if Np == 1:
+        # 用取最大的三个峰值求加权平均
+        Amp = 0
+        WeightedSum = 0
 
-        highestPeak = np.argsort(-np.abs(filteredHs[idx]))
-        if (minIndex in idx[highestPeak][:Np] or maxIndex in idx[highestPeak][:Np]) and BPMrangeI < len(BPMrange):
-            continue
+        for rx in range(Nrx):
+            for tx in range(Ntx):
+                for sc in range(Nsc):
+                    for BPMrangeI in range(len(BPMrange)):
+                        minBPM, maxBPM = BPMrange[BPMrangeI]
+                        minIndex, maxIndex = int(minBPM / BPMresol), int(maxBPM / BPMresol)
+                        filteredHs = np.zeros_like(Hs, dtype=np.float64)
+                        filteredHs[minIndex: maxIndex] = np.abs(H[rx][tx][sc][minIndex:maxIndex])
+                        idx, _ = find_peaks(filteredHs, height = 0.001, distance = 1)
+                        if len(idx) < 1:
+                            continue
+                        highestPeak = np.argsort(-filteredHs[idx])
+                        Amp += filteredHs[idx[highestPeak[0]]]
+                        WeightedSum += filteredHs[idx[highestPeak[0]]] * idx[highestPeak[0]]
+                        if len(idx) > 1:
+                            Amp += filteredHs[idx[highestPeak[1]]]
+                            WeightedSum += filteredHs[idx[highestPeak[1]]] * idx[highestPeak[1]]
+                        if len(idx) > 2:
+                            Amp += filteredHs[idx[highestPeak[2]]]
+                            WeightedSum += filteredHs[idx[highestPeak[2]]] * idx[highestPeak[2]]
+                        break
 
-        ret = np.asarray(idx[highestPeak][:Np] * BPMresol)
+        ret = np.array([WeightedSum / Amp * BPMresol])
+    else:
+        while len(ret) < Np:
+            minBPM, maxBPM = BPMrange[BPMrangeI]
+            BPMrangeI += 1
+            minIndex, maxIndex = int(minBPM / BPMresol), int(maxBPM / BPMresol)
+            filteredHs[minIndex: maxIndex] = Hs[minIndex:maxIndex]
+            idx, _ = find_peaks(np.abs(filteredHs), distance = 5)
+
+            highestPeak = np.argsort(-np.abs(filteredHs[idx]))
+            if (minIndex in idx[highestPeak][:Np] or maxIndex in idx[highestPeak][:Np]) and BPMrangeI < len(BPMrange):
+                continue
+
+            ret = np.asarray(np.sort(idx[highestPeak][:Np]) * BPMresol)
     return ret
